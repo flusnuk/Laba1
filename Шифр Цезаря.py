a09c19127f92080a1355fccce94773d652b99186
flusnuk <90822823+flusnuk@users.noreply.github.com>
2021-09-25 12:31:11 +0300

    Update Шифр Цезаря.py

--- Шифр Цезаря.py
+++ Шифр Цезаря.py
@@ -1,28 +1,15 @@
-#  закоментований код - це код для шифрування слова на англійській мові
-
-import random
-# alfavit_EU =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alfavit_UA = 'АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯАБВ'
-shifr = random.randint(1,10)
-message = input("Повідомлення для дешифровки: ").upper()
-res = ''
-lang = 'UA'
-# lang = input('Виберіть мову UA/EU: ')
-if lang == 'UA':
-    for i in message:
-        mr = alfavit_UA.find(i)
-        new_mr = mr + shifr
-        if i in alfavit_UA:
-            res += alfavit_UA[new_mr]
-        else:
-            res += i
-#else:
-#    for i in message:
- #       mr = alfavit_EU.find(i)
-  #      new_mr = mr + shifr
-   #     if i in alfavit_EU:
-   #         res += alfavit_EU[[new_mr]
-    #    else:
-   #         res += i
-print (res)
-print(shifr)
+alfavit = 'АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯАБВ ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789123'
+shifr = 1
+while True:
+    message = str(input("Повідомлення для дешифровки: ")).upper()
+    res = ''
+    lang = 'UA'
+    if lang == 'UA':
+            for i in message:
+                mr = alfavit.find(i)
+                new_mr = mr + shifr
+                if i in alfavit:
+                    res += alfavit[new_mr]
+                else:
+                    res += i
+    print(res)
